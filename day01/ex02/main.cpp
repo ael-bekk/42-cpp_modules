7e2231197d82e04eac1b13c80c2447e69da8bc78
ael-bekk <abekali451@gmail.com>
2022-11-24 12:54:10 +0100

    day01

---
+++ day01/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string *ptr = &str;
+    std::string &ref = str;
+
+    std::cout << &str << std::endl;
+    std::cout << &ptr << std::endl;
+    std::cout << &ref << std::endl;
+
+    std::cout << str << std::endl;
+    std::cout << *ptr << std::endl;
+    std::cout << ref << std::endl;
+    return (0);
+}
